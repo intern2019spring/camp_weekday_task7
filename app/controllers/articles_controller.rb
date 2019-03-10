5f08af4c7613cf48ae339d82253458d8ce35f75c
Yamamotonasu <deep.blue.deep@outlook.jp>
2019-03-10 21:07:47 +0900

    論文の登録機能追加

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,14 +6,24 @@ class ArticlesController < ApplicationController
   end
 
   def new
+    @article = Article.new
   end
 
   def edit
   end
 
   def create
+    @article = Article.new(article_params)
+    @article.save
+    redirect_to @article
   end
 
   def destroy
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :body)
   end
 end
